d2e02278f9ae7b4e655ad58b403144bb7742eaba
Erik Ihrén <erikihr@gmail.com>
2015-11-08 15:38:56 +0100

    Added jasper stuff

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 def get_light_status():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(("localhost", 5432))
-    status = sock.recv(1024)
+    status = sock.recv(1024) == "True"
 
-    return jsonify({'status':status})
+    return jsonify({'status': status})
 
 @app.route('/nexus/api/lifx/status', methods=["POST"])
 def set_light_status():
@@ -27,8 +27,40 @@ def set_light_status():
     else:
         sock.send("off")
         status = False
-    return jsonify({"status":status}), 201
+    return jsonify({"status": status}), 201
 
+def jasper_status():
+    cmd = os.popen("ps aux | grep jasper")
+    status = "jasper.py" in cmd.read()
+    return status
+
+def restart_jasper():
+    os.system("restart ../jasper/jasper.py")
+
+@app.route('/nexus/api/jasper/status', methods=["GET"])
+def get_jasper_status():
+    return jsonify({"status": jasper_status()})
+
+@app.route('/nexus/api/jasper/status', methods=["POST"])
+def set_jasper_status():
+    if not request.json or not "status" in request.json:
+        abort(400)
+    
+    if request.json["status"]:
+        force = False # Should we force restart?
+        if "force" in request.json and request.json["force"] == True:
+            force = True
+            
+        currently_on = jasper_status()
+
+        if not currently_on or force:
+            restart_jasper()
+            
+            
+        return jsonify({"status": True})
+    else:
+        os.system("pkill -f jasper.py")
+        return jsonify({"status": False})
 
 
 
